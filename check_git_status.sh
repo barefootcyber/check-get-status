c3172c4b0de2201c4a09ddf85df60586eab59807
Barefootcyber <98751540+barefootcyber@users.noreply.github.com>
2023-06-19 14:10:29 +0200

    run add, commit and push

--- check_git_status.sh
+++ check_git_status.sh
@@ -5,7 +5,10 @@ status=$(git status)
 
 # Check if the status matches the desired condition
 if [[ $status =~ "On branch latestcode" && $status =~ "Your branch is up to date with 'origin/latestcode'." && $status =~ "nothing to commit, working tree clean" ]]; then
-    echo "latestcode is up to date."
+    echo "latestcode is up to date. Exiting..."
 else
+    echo "latestcode needs updating. Updating..."
     git add .
+    git commit -m "barefoot from cordei success 3"
+    git push origin latestcode
 fi
